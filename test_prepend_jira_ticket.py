7dc250cee8f530a9769844f26c3e4ef2fecd6723
Ben Falk <falk.ben@gmail.com>
2022-09-15 23:20:56 -0400

    add a test on issue extract
    add dependencies

--- test_prepend_jira_ticket.py
+++ test_prepend_jira_ticket.py
@@ -1,21 +1,24 @@
+import pytest
+
 import prepend_jira_issue
 
 
-def test_get_branch_name():
-    ...
-
-
-def test_extract_jira_issue():
-    ...
-
-
-def test_get_commit_msg():
-    ...
-
-
-def test_write_commit_msg():
-    ...
-
-
-def test_ignore_merge_commit():
-    ...
+@pytest.mark.parametrize(
+    ("content", "expected"),
+    (
+        ("ABC-1111", "ABC-1111"),
+        ("A-1111", None),
+        ("ABC-1", "ABC-1"),
+        ("abc 1111", None),
+        ("two little bears, sitting in chairs", None),
+        ("and a bowl of much 123-ABC", None),
+        ("and a little old lady CDE-123", "CDE-123"),
+        ("CDE-123: and a little old lady ", "CDE-123"),
+        ("ZYX-123-321-ABC", "ZYX-123"),
+        ("ZYX-123-ABC-321", "ZYX-123"),
+        ("ZYX-ABC-321-123", "ABC-321"),
+    ),
+)
+def test_extract_jira_issue(content, expected):
+    output = prepend_jira_issue.extract_jira_issue(content)
+    assert output == expected
